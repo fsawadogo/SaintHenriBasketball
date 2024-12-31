4dac80b1ceb96ed469b0776936059d97fb1e4124
Faïçal Sawadogo <afaicals@gmail.com>
2024-12-31 11:12:01 -0500

    Add swagger to production

--- SaintHenriBasketball.API/Program.cs
+++ SaintHenriBasketball.API/Program.cs
@@ -95,14 +95,20 @@ using (var scope = app.Services.CreateScope())
 }
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI(c =>
+//    {
+//        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Basketball Training API v1");
+//    });
+//}
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Basketball Training API v1");
-    });
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Basketball Training API v1");
+});
 
 app.UseHttpsRedirection();
 
