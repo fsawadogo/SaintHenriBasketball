1f5ca886bda4312ca126956a4b1769b1030cd28f
Faïçal Sawadogo <afaicals@gmail.com>
2025-03-05 07:38:27 -0500

    Hangfire migration files

---
+++ SaintHenriBasketball.Infrastructure/Migrations/20250126160043_AddHangfireSchemaAndPaymentUpdate.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SaintHenriBasketball.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddHangfireSchemaAndPaymentUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Location",
+                table: "Sessions",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Payments",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Reference",
+                table: "Payments",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Payments");
+
+            migrationBuilder.DropColumn(
+                name: "Reference",
+                table: "Payments");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Location",
+                table: "Sessions",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
